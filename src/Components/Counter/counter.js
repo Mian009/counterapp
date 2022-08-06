da8410ad1880cdd6fc2acf926a2a9e715a58f2d1
Mian009 <100489826+Mian009@users.noreply.github.com>
2022-08-07 01:49:22 +0500

    Create counter.js

---
+++ src/Components/Counter/counter.js
@@ -0,0 +1,46 @@
+// var developer;
+import React, { useState } from 'react';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    // console.log(developer());
+
+    // const developer = function () {
+    //     console.log("Mian Mubashir");
+    // }
+    // console.log(developer)
+
+    // function developer(){
+    //     console.log("Mian Mubashir");
+    // }
+
+    // const obj = {
+    //     boy: "Mubashir",
+    //     job: "developer",
+    // }
+
+    // console.log(obj.boy, obj.job);
+    
+    // const { boy, job } = obj;
+
+    // console.log(boy, job);
+
+    // const increment = () => {
+    //     setCount(count + 1);
+    // }
+
+    // const decrement = () => {
+    //     setCount(count - 1);
+    // }
+    
+    return(
+        <>
+        <div>{count}</div>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </>
+
+    )
+}
+
+export default Counter; 
